5b0c962e4c8f883b1fdcfdb5d1e67619f9036dfb
Samantha <uzume.tennouboshi@outlook.com>
2020-11-19 00:45:56 +0000

    Update build to cooperate with jcenter's project requirements

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+[ ![Download](https://api.bintray.com/packages/x-samantha/discord/CommandsJDA/images/download.svg) ](https://bintray.com/x-samantha/discord/CommandsJDA/_latestVersion)
 # CommandsJDA
 CommandsJDA rewritten
--- src/main/kotlin/x/samantha/commandsjda/util/CommandsJDAInfo.kt
+++ src/main/kotlin/x/samantha/commandsjda/util/CommandsJDAInfo.kt
@@ -4,6 +4,6 @@ object CommandsJDAInfo {
     val GITHUB = "https://github.com/x-Samantha/CommandsJDA"
     val VERSION_MAJOR = "0"
     val VERSION_MINOR = "1"
-    val VERSION_REVISION = "-SNAPSHOT"
+    val VERSION_REVISION = "4"
     val VERSION = "$VERSION_MAJOR.$VERSION_MINOR.$VERSION_REVISION"
 }
